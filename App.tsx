db43eb3008ff6714312e7f8b9a72f8532e373ca9
Dalifo <141125340+FlorianPME@users.noreply.github.com>
2023-11-07 16:40:45 +0100

    first commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text 
+      style={styles.title}>Select Your</Text>
+      <Image
+      style={styles.backgroundpoke}
+      source={require('./assets/backpoke.png')}></Image>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  backgroundpoke: {
+    width: 478.21008,
+    height: 478.21008,
+  },
+  title: {
+    color: 'white',
+  }
 });
